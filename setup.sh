44de839ca9b99c00626fdc225634802cce05ef83
GuestyTheBesty <guestythebesty841@gmail.com>
2025-07-21 13:29:23 -0400

    few chnages

--- setup.sh
+++ setup.sh
@@ -32,12 +32,14 @@ yay -S spotify
 
 LOCATION=$(pwd)
 mkdir -p ~/.config/hypr ~/Scripts ~/.config/greetd ~/.config/waybar ~/.config/swaync ~/.config/wofi
-cp -r $LOCATION/hyprland ~/.config/hypr
-cp -r $LOCATION/Scripts ~/Scripts
-cp -r $LOCATION/greetd-tuigreet ~/.config/greetd
-cp -r $LOCATION/waybar ~/.config/waybar
-cp -r $LOCATION/swaync ~/.config/swaync
-cp -r $LOCATION/wofi ~/.config/wofi
+cp -r $LOCATION/hypr/* ~/.config/hypr
+cp -r $LOCATION/Scripts/* ~/Scripts
+cp -r $LOCATION/greetd-tuigreet/* ~/.config/greetd
+cp -r $LOCATION/waybar/* ~/.config/waybar
+cp -r $LOCATION/swaync/* ~/.config/swaync
+cp -r $LOCATION/wofi/* ~/.config/wofi
 
 sudo systemctl --user enable pipewire pipewire-pulse wireplumber
 sudo systemctl enable greetd
+
+reboot
