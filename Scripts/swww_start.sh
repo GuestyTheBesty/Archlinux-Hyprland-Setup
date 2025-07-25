960cd7e888d998f9530906f1b04b6aaea4f8d967
GuestyTheBesty <guestythebesty841@gmail.com>
2025-07-25 01:14:43 -0400

    Update swww_start.sh

--- Scripts/swww_start.sh
+++ Scripts/swww_start.sh
@@ -4,7 +4,7 @@ CUR=$(brightnessctl get)
 brightnessctl set 0% # Prevent hyprland's inherent wallpaper
 
 swww-daemon & # Start swww in background
-$HOME/Scripts/alternate_scenery_wallpapers.sh &
+$HOME/Scripts/alternate_wallpapers.sh &
 sleep 1 # Wait till swww is up
 
 brightnessctl set $CUR 
